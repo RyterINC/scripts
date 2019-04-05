9a9f75397848dad7f12d0d6373956a7765002838
Jayson Ryter <jjryter1@gmail.com>
2019-04-05 19:40:53 -0400

    iniital commit
    - script install basic packages + docker and docker compose

---
+++ ubuntu-intial-setup.sh
@@ -0,0 +1,21 @@
+apt-get update
+apt-get upgrade
+apt-get install vim \
+git
+
+# install docker
+apt-get install \
+apt-transport-https \
+ca-certificates \
+curl \
+software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+
+#install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
